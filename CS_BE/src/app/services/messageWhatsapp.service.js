08492983690b3527b6a82f0a0f96fe295ea302fe
Le-Loc <leeloc1069@gmail.com>
2024-11-05 10:48:28 +0700

    whatsapp

--- CS_BE/src/app/services/messageWhatsapp.service.js
+++ CS_BE/src/app/services/messageWhatsapp.service.js
@@ -1,24 +1,25 @@
-const axios = require("axios");
+const axios = require('axios');
 
 const sendWhatsAppMessage = async (messageData) => {
   try {
     const response = await axios({
-      url: "https://graph.facebook.com/v20.0/505712559281612/messages",
-      method: "post",
+      url: 'https://graph.facebook.com/v20.0/480052998521677/messages',
+      method: 'post',
       headers: {
-        Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-        "Content-Type": "application/json",
+        'Authorization': `Bearer ${process.env.WHATSAPP_TOKEN}`,
+        'Content-Type': 'application/json'
       },
-      data: messageData,
+      data: messageData
     });
 
     return response.data;
   } catch (error) {
-    console.error("Error sending message:", error.message);
-    throw new Error("Failed to send message");
+    console.error('Error sending message:', error.message);
+    throw new Error('Failed to send message');
   }
 };
 
+
 module.exports = {
-  sendWhatsAppMessage,
+  sendWhatsAppMessage
 };
